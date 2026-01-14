d930c0602c5623159859bca6d7f0454a010a7d03
Rizky Delfa <rizkydelfa05@gmail.com>
2026-01-14 17:11:11 +0700

    terakhir

--- admin.php
+++ admin.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['username'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kuliner Nusantara</title>
-    <link rel="icon" href="img/Logo Kuliner.png" type="image/x-icon">
+    <link rel="icon" href="img/Logo Kuliner.png">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
@@ -61,11 +61,25 @@ if (!isset($_SESSION['username'])) {
                 <a class="nav-link dropdown-toggle text-danger fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <?= $_SESSION['username']?>
                 </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="admin.php?page=profile">Profile</a></li> 
-                    <li><a class="dropdown-item" href="logout.php">Logout</a></li> 
-                </ul>
-            </li> 
+                <ul class="dropdown-menu dropdown-menu-end">
+
+            <!-- PROFILE -->
+            <li>
+              <a class="dropdown-item" href="admin.php?page=profile">
+                <i class="bi bi-person me-1"></i> Profile
+              </a>
+            </li>
+
+            <li><hr class="dropdown-divider"></li>
+
+            <!-- LOGOUT -->
+            <li>
+              <a class="dropdown-item text-danger" href="logout.php">
+                <i class="bi bi-box-arrow-right me-1"></i> Logout
+              </a>
+            </li>
+
+          </ul>
         </ul>
         </div>
     </div>
