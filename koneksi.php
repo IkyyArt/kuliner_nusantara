c1e701b8be5e749fd625c9a844fbbed10ee0ecba
Ikyy Web <rizkydelfa05@gmail.com>
2026-01-14 14:50:50 +0700

    Change database connection details
    Updated database connection parameters for new server.

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,10 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "tumbal kuliner"; //nama database
+$servername = "sql101.infinityfree.com";
+$username = "if0_40853086";
+$password = "aftstS1gigES";
+$db = "if0_40853086_kulinernusantara"; //nama database
 
 //create connection
 $conn = new mysqli($servername,$username,$password,$db);
